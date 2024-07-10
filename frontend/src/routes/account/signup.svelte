0f25ac26348720bde09e8f1c27c4b4dce34ed535
Theophilus <theophilus.ogbolu@gmail.com>
2024-07-10 23:07:51 +0100

    fix loading

--- frontend/src/routes/account/signup.svelte
+++ frontend/src/routes/account/signup.svelte
@@ -68,7 +68,7 @@
 	const submit = async () => {
 		form.email_template = email_template.innerHTML.replace(/&amp;/g, '&');
 
-		// $loading = 'Loading . . .';
+		$loading = 'Loading . . .';
 		let resp = await fetch(`${import.meta.env.VITE_BACKEND}/signup`, {
 			method: 'post',
 			headers: {
