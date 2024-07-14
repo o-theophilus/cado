b908326036f2f44381289174fd92583104261a60
Theophilus <theophilus.ogbolu@gmail.com>
2024-07-14 13:10:18 +0100

    icon

--- frontend/src/routes/layout/icon.svelte
+++ frontend/src/routes/layout/icon.svelte
@@ -8,8 +8,29 @@
 			'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200';
 		link.rel = 'stylesheet';
 		link.onload = () => {
-			$icon_loaded = true;
+			setTimeout(() => {
+				check();
+			}, 100);
 		};
 		document.head.appendChild(link);
 	});
+
+	const check = () => {
+		const iconElement = document.getElementById('icon-check');
+		const computedStyle = window.getComputedStyle(iconElement, null);
+		if (computedStyle.getPropertyValue('font-family').includes('Material Symbols Outlined')) {
+			$icon_loaded = true;
+		} else {
+			$icon_loaded = false;
+		}
+	};
 </script>
+
+<span id="icon-check" class="material-symbols-outlined"> check </span>
+
+<style>
+	.material-symbols-outlined {
+		/* visibility: hidden; */
+		display: none;
+	}
+</style>
