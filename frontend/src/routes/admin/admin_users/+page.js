4ee81f2f79a556a14c0ca314f69fc1425e47efca
Theophilus <theophilus.ogbolu@gmail.com>
2025-07-20 16:49:22 +0100

    fix

--- frontend/src/routes/admin/admin_users/+page.js
+++ frontend/src/routes/admin/admin_users/+page.js
@@ -1,6 +1,6 @@
 import { error } from '@sveltejs/kit';
 import { get } from 'svelte/store';
-import { state, loading } from "$lib/store.svelte.js"
+import { page_state, loading } from "$lib/store.svelte.js"
 
 export const load = async ({ fetch, url, parent }) => {
 	let a = await parent();
@@ -9,7 +9,7 @@ export const load = async ({ fetch, url, parent }) => {
 	}
 
 	let page_name = "admin_users"
-	let _state = get(state)
+	let _state = get(page_state)
 	let i = _state.findIndex(x => x.name == page_name);
 
 	if (i == -1) {
@@ -17,7 +17,7 @@ export const load = async ({ fetch, url, parent }) => {
 			name: page_name,
 			search: url.search
 		})
-		state.set(_state)
+		page_state.set(_state)
 		i = _state.findIndex(x => x.name == page_name);
 	}
 
